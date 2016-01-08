d2678d7e9a412c64eef5d535825f50bbf60e4aa6
40123256 <40123256@gm.nfu.edu.tw>
2016-01-08 21:13:33 +0800

    grw

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '40123256'
-SITENAME = '2015FALL 40123256 CPA 作業 (期中報告)'
+AUTHOR = '40123157'
+SITENAME = '2015FALL 40123157 CPB 作業 (期中報告)'
 SITEURL = ''
 
 PATH = 'content'
@@ -25,7 +25,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'), ('Python', 'http://python.org/'),  ('2015作業主頁', '../../'), ('[github] 2015fallhw/2015fallcpb', 'https://github.com/2015fallhw/2015fallcpb/tree/gh-pages'), ('[github] 40123256', 'https://github.com/40123256'), ('[vimeo] 40123157', 'https://vimeo.com/user25757242'), ('[github.io] 40123256/cpw', 'http://40123256.github.io/cpw/'))
+LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'), ('Python', 'http://python.org/'),  ('2015作業主頁', '../../'), ('[github] 2015fallhw/2015fallcpb', 'https://github.com/2015fallhw/2015fallcpb/tree/gh-pages'), ('[github] 40123157', 'https://github.com/40123157'), ('[vimeo] 40123157', 'https://vimeo.com/user25757242'), ('[github.io] 40123157/cpbw8', 'http://40123157.github.io/cpbw8/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
